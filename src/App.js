68d7bf5e65c8a63d8c18e3f6e27dc31396547e0c
kec <57247368+yesky85@users.noreply.github.com>
2020-03-18 22:08:06 +0900

    Attributes and Mixins

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React, { Component } from "react";
-import styled, { createGlobalStyle, keyframes, css } from "styled-components";
+import styled, { createGlobalStyle, css } from "styled-components";
 
 const GlobalStyle = createGlobalStyle`
   body {
@@ -8,22 +8,12 @@ const GlobalStyle = createGlobalStyle`
   }
 `;
 
-class App extends Component {
-  render() {
-    return (
-      <Container>
-        <GlobalStyle />
-        <Button>SUCCESS</Button>
-        <Button danger rotationTime={1}>
-          DANGER
-        </Button>
-        <Anchor as="a" href="http://google.co.kr">
-          Go to google
-        </Anchor>
-      </Container>
-    );
-  }
-}
+const awesomeCard = css`
+  box-shadow: 0 4px 6px rgba(50, 50, 93, 0.11), 0 1px 3px rgba(0, 0, 0, 0.08);
+  background-color: white;
+  border-radius: 10px;
+  padding: 20px;
+`;
 
 const Container = styled.div`
   height: 100vh;
@@ -31,39 +21,22 @@ const Container = styled.div`
   background-color: pink;
 `;
 
-const Button = styled.button`
-  border-radius: 50px;
-  padding: 5px;
-  min-width: 120px;
-  color: white;
-  font-weight: 600;
-  -webkit-appearance: none;
-  cursor: pointer;
-  &:active,
-  &:focus {
-    outline: none;
-  }
-  background-color: ${props => (props.danger ? "#e74c3c" : "#2ecc71")};
-  ${props => {
-    if (props.danger) {
-      return css`
-        animation: ${rotation} ${props.rotationTime}s linear infinite;
-      `;
-    }
-  }}
+const Input = styled.input.attrs({
+  required: true
+})`
+  border: none;
+  ${awesomeCard}
 `;
 
-const Anchor = styled(Button)`
-  text-decoration: none;
-`;
-
-const rotation = keyframes`
-  from{
-    transform: rotate(0deg);
+class App extends Component {
+  render() {
+    return (
+      <Container>
+        <GlobalStyle />
+        <Input placeholder="Hello" />
+      </Container>
+    );
   }
-  to{
-    transform: rotate(360deg);
-  }
-`;
+}
 
 export default App;
